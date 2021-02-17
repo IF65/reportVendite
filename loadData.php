8ed5cb31fa1add58c5bbdc5b2d0006ebfc4cc441
Marco Gnecchi <marco.gnecchi@if65.it>
2021-02-17 07:09:44 +0100

    allineamento giornaliero

--- loadData.php
+++ loadData.php
@@ -6,8 +6,8 @@ require 'vendor/autoload.php';
 
 $timeZone = new DateTimeZone('Europe/Rome');
 
-$dataInizio = new DateTime('2020-02-17', $timeZone);
-$dataFine = new DateTime('2020-02-23', $timeZone);
+$dataInizio = new DateTime('2021-02-15', $timeZone);
+$dataFine = new DateTime('2021-02-16', $timeZone);
 
 // identificazione server
 // -------------------------------------------------------------------------------
