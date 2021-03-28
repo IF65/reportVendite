8dcfb55a28f9c32c89fa0fcabbce6274ba418c42
Marco Gnecchi <marco.gnecchi@if65.it>
2021-03-28 05:49:17 +0200

    varie

--- caricaOre.php
+++ caricaOre.php
@@ -15,6 +15,7 @@ $password = "mela";
 
 $timeZone = new DateTimeZone('Europe/Rome');
 $currentDate = new DateTime('now', $timeZone);
+$startingDate = (clone $currentDate)->sub(new DateInterval('P820D'));
 
 // inizio
 // -----------------------------------------------------------
@@ -24,31 +25,55 @@ try {
 	// -------------------------------------------------------------------------------
 	$db = new PDO("mysql:host=$hostname", $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-	// creo la tabella sales sul db destinazione
+	// creazione tabelle
 	// -------------------------------------------------------------------------------
-	$stmt = "	CREATE TABLE IF NOT EXISTS mtx.ore (
-					`sede` varchar(4) NOT NULL DEFAULT '',
-					`data` date NOT NULL,
-					`codiceReparto` tinyint(3) unsigned NOT NULL,
-					`vendutoTotale` decimal(11,2) NOT NULL,
-					`vendutoReparto` decimal(11,2) NOT NULL,
-					`oreReparto` decimal(11,2) NOT NULL,
-				PRIMARY KEY (`sede`,`data`,`codiceReparto`)
+	$stmt = "	CREATE TABLE IF NOT EXISTS mtx.hours (
+				  `store` varchar(4) NOT NULL DEFAULT '',
+				  `ddate` date NOT NULL,
+				  `code` tinyint(3) unsigned NOT NULL,
+				  `amount` decimal(11,2) NOT NULL,
+				  `hours` decimal(11,2) NOT NULL,
+				  PRIMARY KEY (`store`,`ddate`,`code`)
 				) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
-
 	$h_create = $db->prepare($stmt);
 	$h_create->execute();
 
-	$stmt = "	insert into mtx.ore 
-					(`sede`,`data`,`codiceReparto`,`vendutoTotale`,`vendutoReparto`,`oreReparto`)
+	$stmt = "	CREATE TABLE IF NOT EXISTS mtx.`control` (
+				  `store` varchar(4) NOT NULL DEFAULT '',
+				  `ddate` date NOT NULL,
+				  `totalamount` decimal(11,2) NOT NULL,
+				  `totalhours` decimal(11,2) NOT NULL,
+				  `customercount` int(10) unsigned NOT NULL,
+				  `closed` tinyint(3) unsigned NOT NULL,
+				  PRIMARY KEY (`store`,`ddate`)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+	$h_create = $db->prepare($stmt);
+	$h_create->execute();
+
+	$stmt = "	insert into mtx.hours 
+					(`store`,`ddate`,`code`,`amount`,`hours`)
 				values
-					(:sede,:data,:codiceReparto,:vendutoTotale,:vendutoReparto,:oreReparto)
+					(:store,:ddate,:code,:amount,:hours)
 				on duplicate key update 
-				    `codiceReparto`=:codiceReparto,
-				    `vendutoTotale`=:vendutoTotale,
-				    `vendutoReparto`=:vendutoReparto,
-				    `oreReparto`=:oreReparto";
-	$h_insert = $db->prepare($stmt);
+				    `code`=:code,
+				    `amount`=:amount,
+				    `hours`=:hours";
+	$h_insert_hours = $db->prepare($stmt);
+
+	$stmt = "	insert into mtx.control 
+					(`store`,`ddate`,`totalamount`,`totalhours`,`customercount`,`closed`)
+				values
+					(:store,:ddate,:totalamount,:totalhours,:customercount,:closed)
+				on duplicate key update 
+				    `totalamount`=:totalamount,
+				    `totalhours`=:totalhours,
+				    `customercount`=:customercount,
+					`closed`=:closed";
+	$h_insert_control = $db->prepare($stmt);
+
+	$stmt = "	update mtx.control as c join (select store, ddate, sum(hours) hours from mtx.hours group by 1,2) as h on c.store=h.store and c.ddate=h.ddate 
+				set c.totalhours = h.hours where h.ddate >= :ddate";
+	$h_update_control = $db->prepare($stmt);
 
 	$client = new Client([
 		'base_uri' => 'http://10.11.14.74/',
@@ -60,7 +85,7 @@ try {
 		['json' =>
 			[
 				'dataFine' => $currentDate->format(DATE_ATOM),
-				'dataInizio' => $currentDate->sub(new DateInterval('P7D'))->format(DATE_ATOM),
+				'dataInizio' => $startingDate->format(DATE_ATOM),
 				'function' => 'caricaOreLavorate'
 			]
 		]
@@ -70,15 +95,24 @@ try {
 		$reparti = json_decode($response->getBody()->getContents(), true);
 		if (isset($reparti)) {
 			foreach ($reparti as $reparto) {
-				$h_insert->execute([
-					':sede' => $reparto['sede'],
-					':data' => (new DateTime($reparto['data'], $timeZone))->format('Y-m-d'),
-					':codiceReparto' => $reparto['codiceReparto'],
-					':vendutoTotale' => $reparto['vendutoTotale'],
-					':vendutoReparto' => $reparto['vendutoReparto'],
-					':oreReparto' => $reparto['oreReparto']
+				$h_insert_hours->execute([
+					':store' => $reparto['store'],
+					':ddate' => (new DateTime($reparto['ddate'], $timeZone))->format('Y-m-d'),
+					':code' => $reparto['code'],
+					':amount' => $reparto['amount'],
+					':hours' => $reparto['hours']
+				]);
+
+				$h_insert_control->execute([
+					':store' => $reparto['store'],
+					':ddate' => (new DateTime($reparto['ddate'], $timeZone))->format('Y-m-d'),
+					':totalamount' => $reparto['totalamount'],
+					':totalhours' => 0,
+					':customercount' => $reparto['customercount'],
+					':closed' => $reparto['closed']
 				]);
 			}
+			$h_update_control->execute([':ddate' => ($startingDate->format('Y-m-d'))]);
 		}
 	} else {
 		echo "\n";
